5cc9b2fac008b01def21af53ed43be55fabc511b
afandiakbar <49123055+afandiakbar@users.noreply.github.com>
2019-10-16 20:28:48 +0800

    Create Matematika.py

---
+++ Matematika.py
@@ -0,0 +1,15 @@
+def penjumlahan(bil1,bil2):
+  hasil = bi11 + bi12
+  print("Hasil dari matematika sederhana",bi11,"+",bi12,"=",hasil)
+  
+def perkalian(bi11, bi12):
+  hasil = bi11 * bi12
+  print("Hasil dari matematika sederhana",bi11,"*"bi12,"=",hasil)
+    
+def pengurangan(bi11, bi12):
+  hasil = bi11 - bi12
+  print("Hasil dari matematika sederhana",bi11,"-",bi12,"="hasil)
+    
+def pembagian(bi11, bi12):
+  hasil = bi11 / bi12
+  print("Hasil dari matematika sederhana",bi11,"/",bi12,"=",hasil)
